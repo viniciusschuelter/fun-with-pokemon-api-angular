1608b269472a09c2511ce2186568b6930e74efbd
viniciusschuelter <viniciusschuelter@gmail.com>
2021-04-11 16:49:48 -0300

    fix(build): build netlify

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/comicBooks'),
+      dir: require('path').join(__dirname, './coverage/funWithPokeapi'),
       subdir: '.',
       reporters: [
         { type: 'html' },
