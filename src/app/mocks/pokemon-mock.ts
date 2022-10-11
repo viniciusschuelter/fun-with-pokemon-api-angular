df7f5842896857081a8b52f36c032971177949e8
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2022-10-11 19:07:21 -0300

    chore(details): fix some details

---
+++ src/app/mocks/pokemon-mock.ts
@@ -0,0 +1,125 @@
+import { PokemonMini } from '../models/interfaces';
+
+export const pokemonTypes: PokemonMini[] = [
+  {
+    "name": "normal",
+    "url": "https://pokeapi.co/api/v2/type/1/"
+  },
+  {
+    "name": "fighting",
+    "url": "https://pokeapi.co/api/v2/type/2/"
+  },
+  {
+    "name": "flying",
+    "url": "https://pokeapi.co/api/v2/type/3/"
+  },
+  {
+    "name": "poison",
+    "url": "https://pokeapi.co/api/v2/type/4/"
+  },
+  {
+    "name": "ground",
+    "url": "https://pokeapi.co/api/v2/type/5/"
+  },
+  {
+    "name": "rock",
+    "url": "https://pokeapi.co/api/v2/type/6/"
+  },
+  {
+    "name": "bug",
+    "url": "https://pokeapi.co/api/v2/type/7/"
+  },
+  {
+    "name": "ghost",
+    "url": "https://pokeapi.co/api/v2/type/8/"
+  },
+  {
+    "name": "steel",
+    "url": "https://pokeapi.co/api/v2/type/9/"
+  },
+  {
+    "name": "fire",
+    "url": "https://pokeapi.co/api/v2/type/10/"
+  },
+  {
+    "name": "water",
+    "url": "https://pokeapi.co/api/v2/type/11/"
+  },
+  {
+    "name": "grass",
+    "url": "https://pokeapi.co/api/v2/type/12/"
+  },
+  {
+    "name": "electric",
+    "url": "https://pokeapi.co/api/v2/type/13/"
+  },
+  {
+    "name": "psychic",
+    "url": "https://pokeapi.co/api/v2/type/14/"
+  },
+  {
+    "name": "ice",
+    "url": "https://pokeapi.co/api/v2/type/15/"
+  },
+  {
+    "name": "dragon",
+    "url": "https://pokeapi.co/api/v2/type/16/"
+  },
+  {
+    "name": "dark",
+    "url": "https://pokeapi.co/api/v2/type/17/"
+  },
+  {
+    "name": "fairy",
+    "url": "https://pokeapi.co/api/v2/type/18/"
+  },
+  {
+    "name": "unknown",
+    "url": "https://pokeapi.co/api/v2/type/10001/"
+  },
+  {
+    "name": "shadow",
+    "url": "https://pokeapi.co/api/v2/type/10002/"
+  }
+];
+
+export const pokemonHabitats: PokemonMini[] = [
+  {
+    "name": "cave",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/1/"
+  },
+  {
+    "name": "forest",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/2/"
+  },
+  {
+    "name": "grassland",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/3/"
+  },
+  {
+    "name": "mountain",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/4/"
+  },
+  {
+    "name": "rare",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/5/"
+  },
+  {
+    "name": "rough-terrain",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/6/"
+  },
+  {
+    "name": "sea",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/7/"
+  },
+  {
+    "name": "urban",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/8/"
+  },
+  {
+    "name": "waters-edge",
+    "url": "https://pokeapi.co/api/v2/pokemon-habitat/9/"
+  }
+]
+
+
