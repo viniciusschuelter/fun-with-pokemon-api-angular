55228876fa905674d5a795b1540f18d1dd3c7784
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2023-11-19 14:54:27 -0300

    feat(cf-syntax): feat control flow syntax in progress

--- src/app/components/pokemon/pokemon-stats/pokemon-stats.component.ts
+++ src/app/components/pokemon/pokemon-stats/pokemon-stats.component.ts
@@ -1,25 +1,26 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { PokemonStat } from 'src/app/models/interfaces';
-import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-pokemon-stats',
   template: `
-    <div class="d-flex align-items-center" *ngFor="let item of pokemonStats">
-      <span class="label stats pr-2">{{
-        statsLabel[item.stat?.name] | uppercase
-      }}</span>
-      <div class="progress m-2 w-100">
-        <div
-          class="progress-bar {{ item.stat?.name }}"
-          [style.width]="item?.base_stat * 0.65 + '%'"
-        ></div>
+    @for (item of pokemonStats; track item.stat?.name) {
+      <div class="d-flex align-items-center">
+        <span class="label stats pr-2 text-uppercase">
+        {{ statsLabel[item.stat?.name] }}
+        </span>
+        <div class="progress m-2 w-100">
+          <div
+            class="progress-bar {{ item.stat?.name }}"
+            [style.width]="item?.base_stat * 0.65 + '%'"
+          ></div>
+        </div>
+        <span class="label number">{{ item?.base_stat }}</span>
       </div>
-      <span class="label number">{{ item?.base_stat }}</span>
-    </div>
+    }
+
   `,
-  imports: [CommonModule],
   styleUrls: ['./pokemon-stats.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
--- src/app/components/pokemon/pokemon-type/pokemon-type.component.ts
+++ src/app/components/pokemon/pokemon-type/pokemon-type.component.ts
@@ -1,19 +1,32 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { PokemonTypesEnum } from 'src/app/models/interfaces';
 import { AwesomeTooltipModule } from '../../../directives/tooltip/tooltip.module';
+import { typePokemonColors } from '../../../mocks/pokemon-mock';
 
 @Component({
   standalone: true,
   selector: 'app-pokemon-type',
   template: `
-    <div class="icon badge badge-pill p-1 {{ type }}" [awesomeTooltip]="type">
-      <img src="./assets/icons/svg/{{ type }}.svg" />
+    <div class="icon badge badge-pill p-1" [style.backgroundColor]="typeColors[type]" [awesomeTooltip]="type">
+      <img class="img" src="./assets/icons/svg/{{ type }}.svg" />
     </div>
   `,
   imports: [AwesomeTooltipModule],
-  styleUrls: ['./pokemon-type.component.scss'],
+  styles: `
+    :host {
+      display: block;
+
+      .img {
+          width: 20px;
+          height: 20px;
+      }
+    }
+
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PokemonTypeComponent {
   @Input() type: PokemonTypesEnum;
+
+  typeColors = typePokemonColors;
 }
